84ba793df822a1f20e243e61fbf264cb1bd9c392
Jonatan Männchen <jonatan@maennchen.ch>
2026-08-30 08:23:20 -0400

    Release v0.3.5

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Varsel.MixProject do
   def project do
     [
       app: :varsel,
-      version: "0.3.4",
+      version: "0.3.5",
       elixir: "~> 1.15",
       elixirc_options: [warnings_as_errors: true],
       elixirc_paths: elixirc_paths(Mix.env()),
